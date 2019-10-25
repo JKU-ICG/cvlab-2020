5cf4c7f5720a3d41b1bfcbc710b9af89f0c4f723
schedldave <david.schedl@gmail.com>
2019-10-25 14:53:36 +0200

    Lab 02 camera calibration

--- lab_02/camera_calibration.m
+++ lab_02/camera_calibration.m
@@ -5,9 +5,9 @@ clear; close all; clc; % clean up!
 
 
 % RGB images:
-% WARNING: images are quite large and thus calibration takes time
-% imgData = imageDatastore( fullfile( 'data/calibration/RGB/' ) );
 % DOWNLOAD from: https://drive.google.com/open?id=1sn5okDv9zIt2ieGDdhi8-QqPwrsDI4-P
+% NOTE: images are quite large and thus calibration takes time
+% imgData = imageDatastore( fullfile( 'data/calibration/RGB/' ) );
 
 % thermal images: (lower resolution)
 imgData = imageDatastore( fullfile( 'data/calibration/thermal/' ) );
@@ -39,15 +39,16 @@ worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 
 
 % View reprojection errors
-h1=figure; showReprojectionErrors(cameraParams);
+h1=figure(1); showReprojectionErrors(cameraParams);
 
 % Visualize pattern locations
-h2=figure; showExtrinsics(cameraParams, 'CameraCentric');
+h2=figure(2); showExtrinsics(cameraParams, 'CameraCentric');
 
 % Display parameter estimation errors
 displayErrors(estimationErrors, cameraParams);
 
 % Store for later usage:
+if ~exist( 'results', 'dir' ), mkdir( 'results' ); end;
 if ~isempty(strfind( imgData.Files{1}, 'RGB' ))
     save( 'results/camParams_RGB.mat', 'cameraParams', 'imageFileNames', 'estimationErrors' );
 elseif ~isempty(strfind( imgData.Files{1}, 'thermal' ))
@@ -56,6 +57,46 @@ end
 
 %% For example, you can use the calibration data to remove effects of lens distortion.
 undistortedImage = undistortImage(originalImage, cameraParams);
-figure; subplot(1,2,1); imshow( originalImage ); title( 'original' );
+figure(3); subplot(1,2,1); imshow( originalImage ); title( 'original' );
 subplot(1,2,2); imshow( undistortedImage ); title( 'undistored' );
+
+%% Project 3D points on checkerboard
+
+worldPts3D = worldPoints; % cornerpoints of the checkerboard
+worldPts3D(:,3) = 0; % add z axis with 0
+
+useId = 8; % pick an image to project to
+rotMatrix = cameraParams.RotationMatrices(:,:,useId); % 3x3 matrix
+translVector = cameraParams.TranslationVectors(useId,:); %1x3 vector
+
+I = imread(imageFileNames{useId});
+U = undistortImage(I, cameraParams);
+
+% reproject 3D points onto image
+imgPtsU = worldToImage(cameraParams,rotMatrix,translVector,worldPts3D,'ApplyDistortion',false);
+imgPtsI = worldToImage(cameraParams,rotMatrix,translVector,worldPts3D,'ApplyDistortion',true);
+
+figure(4); 
+subplot(1,2,1); imshow( I ); title( 'reproject on original (with distortions)' ); hold on;
+plot( imgPtsI(:,1), imgPtsI(:,2), 'bx', 'MarkerSize', 20, 'LineWidth', 3 );
+subplot(1,2,2); imshow( U ); title( 'reproject on undistored' ); hold on;
+plot( imgPtsU(:,1), imgPtsU(:,2), 'bx', 'MarkerSize', 20, 'LineWidth', 3 );
+
+%% manually project 3D points onto image (without worldToImage function)
+
+% bring into camera coordinate system
+camPts = worldPts3D * rotMatrix + translVector;
+
+K = cameraParams.IntrinsicMatrix; % intrinsic camera matrix
+
+% project with intrinsic camera matrix
+tmpPts = camPts * K;
+imgPts = tmpPts(:,1:2) ./ tmpPts(:,3); % perspective division
+
+
+figure(5); 
+imshow( U ); title( 'manual reproject on undistored' ); hold on;
+plot( imgPts(:,1), imgPts(:,2), 'g+', 'MarkerSize', 20, 'LineWidth', 3 );
+
+
 
